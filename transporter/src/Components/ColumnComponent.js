6d9725f0836741dccc3419ac4ec1204ff493c50d
omar jandali <omarjandali93@gmail.com>
2022-11-10 01:58:28 -0800

    Omar Jandali: Setup an icon and name section; created a simple input field for the from and to

--- transporter/src/Components/ColumnComponent.js
+++ transporter/src/Components/ColumnComponent.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import icon from '../assets/underground.svg'
+import '../Styles/Column.css'
 
 const ColumnComponent = () => {
+
+  const [from, setFrom] = useState('')
+  const [to, setTo] = useState('')
+
   return (
-    <div>ColumnComponent</div>
+    <div className='column-container'>
+      <div className='column-header'>
+        <img alt='train icon' className='app-icon' src={icon}/>
+        <p className='app-name'>Transporter</p>
+      </div>
+      <div className='spacer'></div>
+      <div className='form-container'>
+        <div className='input-row'>
+          <p className='input-label'>From:</p>
+          <input className='input-input' onChange={() => {setFrom()}} type="text" id="from" name="from" value={from}/>
+        </div>
+        <div className='input-row'>
+          <p className='input-label'>To:</p>
+          <input className='input-input' onChange={() => {setTo()}} type="text" id="from" name="from" value={to}/>
+        </div>
+      </div>
+    </div>
   )
 }
 
