2f4706ae5dc3ed99280022b678306d71288bf310
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2022-11-15 10:54:40 -0800

    a

--- transporter/src/Components/MapComponent.js
+++ transporter/src/Components/MapComponent.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
 const MapComponent = () => {
-  return (
-    <div>MapComponent</div>
-  )
+  const render = (status) => {
+    if (status === Status.LOADING) return <h3>{status} ..</h3>;
+    if (status === Status.FAILURE) return <h3>{status} ...</h3>;
+    return null;
+  };
+  
+  function MyMapComponent(
+    map = new window.google.maps.Map(document.getElementById("map"), {
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 8,
+    })
+  ) {
+    const ref = useRef();
+  
+    useEffect(() => {
+      new window.google.maps.Map(ref.current, {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+      });
+    });
+  
+    return <div ref={ref} id="map" />;
+  }
+  
+  const center = { lat: -34.397, lng: 150.644 };
+  const zoom = 4;
+  
+    return (
+      <Wrapper apiKey="AIzaSyDXQt2X96rqbBvX1Fcu7VvWZsDdLd1JLt8" render={render}>
+        <MyMapComponent center={{ lat: -34.397, lng: 150.644 }} zoom={4} />
+      </Wrapper>
+   
+    );
+  
+
 }
+
 
 export default MapComponent
